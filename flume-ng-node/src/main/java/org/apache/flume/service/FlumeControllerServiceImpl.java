7d0c970c33f6050fdaa0f9ba823254b95041b088
suifengzhuliu <zhangjunemail@126.com>
2018-07-31 11:45:54 +0800

    start and stop

--- flume-ng-node/src/main/java/org/apache/flume/service/FlumeControllerServiceImpl.java
+++ flume-ng-node/src/main/java/org/apache/flume/service/FlumeControllerServiceImpl.java
@@ -1,20 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.flume.service;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.flume.lifecycle.LifecycleAware;
+import org.apache.flume.node.Application;
+import org.apache.flume.node.PollingZooKeeperConfigurationProvider;
 import org.apache.flume.service.FlumeControllerService.Iface;
 import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.google.common.eventbus.EventBus;
 
 public class FlumeControllerServiceImpl implements Iface {
+	private static final Logger logger = LoggerFactory.getLogger(FlumeControllerServiceImpl.class);
+	private ConcurrentHashMap<String, Application> map = new ConcurrentHashMap<String, Application>();
 
 	@Override
-	public Status startFlumeAgent(String name, String prop) throws TException {
-		// TODO Auto-generated method stub
-		return null;
+	public Status startFlumeAgent(String agentName, String prop) throws TException {
+
+		try {
+			Application application = null;
+			// get options
+			String zkConnectionStr = "localhost:2181";
+			String baseZkPath = "/flume";
+
+			EventBus eventBus = new EventBus(agentName + "-event-bus");
+			List<LifecycleAware> components = Lists.newArrayList();
+			PollingZooKeeperConfigurationProvider zookeeperConfigurationProvider = new PollingZooKeeperConfigurationProvider(agentName, zkConnectionStr,
+					baseZkPath, eventBus);
+			components.add(zookeeperConfigurationProvider);
+			application = new Application(components);
+			eventBus.register(application);
+
+			application.start();
+
+			map.put(agentName, application);
+
+			final Application appReference = application;
+			Runtime.getRuntime().addShutdownHook(new Thread("agent-shutdown-hook") {
+				@Override
+				public void run() {
+					appReference.stop();
+				}
+			});
+
+		} catch (Exception e) {
+			logger.error("A fatal error occurred while running. Exception follows.", e);
+		}
+
+		return Status.OK;
 	}
 
 	@Override
-	public Status stopFlumeAgent(String name) throws TException {
-		// TODO Auto-generated method stub
-		return null;
+	public Status stopFlumeAgent(String agentName) throws TException {
+		if (map.get(agentName) != null) {
+			map.get(agentName).stop();
+		}
+		return Status.OK;
 	}
 
 }
