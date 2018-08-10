ecb168b54f9e477905cf8a1c07790944423b753e
suifengzhuliu <zhangjunemail@126.com>
2018-08-10 16:00:46 +0800

    thrift

--- flume-ng-node/src/main/thrift/flumecontroller.thrift
+++ flume-ng-node/src/main/thrift/flumecontroller.thrift
@@ -39,6 +39,7 @@ enum SourceType{
 enum SinkType{
 	ES,
 	HDFS,
+	KAFKA,
 	LOGGER
 }
 
@@ -50,13 +51,18 @@ struct HttpSource{
 struct KafkaSource{
 	1: required string servers
 	2: required string topics
-	3: required string group
-	4: optional string topicsRegex
-	5: optional string batchSize
+	3: optional string group
 }
 
 struct HDFSSource{
 	1: required string host
+}
+
+
+enum DBType{
+MYSQL,
+SQLSERVER,
+ORACLE
 }
 
 struct DBSource{
@@ -67,6 +73,8 @@ struct DBSource{
 	5: required string dbName
 	6: required string sql
 	7: required string interval
+	8: required DBType type
+	
 }
 
 
@@ -82,26 +90,42 @@ struct HDFSSink{
 	2: optional string filePrefix
 }
 
+struct KafkaSink{
+	1: required string topic
+	2: required string servers
+}
+
+struct FlumeSource {
+	1:required SourceType sourceType
+	2:optional HttpSource httpSource
+	3:optional KafkaSource kafkaSource
+	4:optional HDFSSource hdfsSource
+	5:optional DBSource dbSource
+	6: optional list<Interceptor> interceptorList
+}
+
+
+struct FlumeSink {
+	1:required SinkType SinkType
+	2:optional HDFSSink hdfsSink
+	3:optional ESSink esSink
+	4:optional KafkaSink kafkaSink
+}
+
+
+struct Interceptor{
+	1:required string type
+	2:optional map<string,string> params
+}
 
 
 
 struct FlumeAgent {                            
  
 1:required string agentName
-2: required SourceType sourceType
-3: required SinkType SinkType
+2: required list<FlumeSource> sourceList
+3: required list<FlumeSink> sinkList
  
-4:optional HttpSource httpSource
-5:optional KafkaSource kafkaSource
-6:optional HDFSSource hdfsSource
-7:optional DBSource dbSource
- 
- 
-8:optional HDFSSink hdfsSink
-9:optional ESSink esSink
- 
- # 区别是否有状态服务,有状态服务只能在一个节点启动成功,无状态服务可以在多个机器启动成功
-10:required bool hasStateService
 }
 
 service   FlumeControllerService {
